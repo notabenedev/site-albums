1348b970cf28b9cc0006ad95146cbd890be0f24d
GIS <dev@gis4biz.ru>
2023-04-07 09:10:18 +0300

    Initial commit

---
+++ src/AlbumsServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Notabenedev\SiteAlbums;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class AlbumsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
